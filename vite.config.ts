5ba3f6097baf1ed1a3a1097519d0d974176c66f3
AllwynThomas <aet@njit.edu>
2026-05-15 11:06:16 -0400

    fix(config): update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/',
+  base: '/is117-career-engineered-portfolio/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
